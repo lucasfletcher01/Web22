340bc7bdee6891310da5784fd4279e9a7e14013a
CatherineBaptist <92572194+CatherineBaptist@users.noreply.github.com>
2022-02-15 10:54:48 +0000

    Update Animation.js

--- js/Animation.js
+++ js/Animation.js
@@ -1,14 +1,5 @@
-const TeamMember = document.querySelector('.TeamMembers');
-TeamMember.classList.remove('TeamAnimation');
-
-const observer = new IntersectionObserver(entries => {
-entries.forEach(entry => {
-if (entry.isIntersecting) {
-TeamMember.classList.add('TeamAnimation');
-  return;
+on window scroll {
+if (scroll pos > x) {
+elements.addClass("TeamAnimation");
 }
-  TeamMember.classList.remove('TeamAnimation');
-});
-});
-
-observer.observe(document.querySelector('.TeamWrapper'));
+}
