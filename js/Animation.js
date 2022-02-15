27bb8ead971e54016045359f5631263d8f8319ff
CatherineBaptist <92572194+CatherineBaptist@users.noreply.github.com>
2022-02-15 20:24:13 +0000

    Update Animation.js

--- js/Animation.js
+++ js/Animation.js
@@ -1,29 +1,29 @@
-function isElementInViewport(elem) {
-var $elem = $(elem);
 
-  var scrollElem = ((navigation.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
-  var viewportTop = $(scrollElem).scrollTop();
-  var elemBottom = viewportTop + $(window).height();
+
+
+// function isElementInViewport(elem) {
+// var $elem = $(elem);
+
+//   var scrollElem = ((navigation.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+//   var viewportTop = $(scrollElem).scrollTop();
+//   var elemBottom = viewportTop + $(window).height();
   
-  var elemTop = Math.round($elem.offset().top);
-  var elemBottom = elemTop + $elem.height();
+//   var elemTop = Math.round($elem.offset().top);
+//   var elemBottom = elemTop + $elem.height();
   
-  return ((elemTop < viewportBottom) && (elemBottom > viewTop));
-}
+//   return ((elemTop < viewportBottom) && (elemBottom > viewTop));
+// }
 
-function checkAnimation() {
-var $elem = $('.bar .level');
+// function checkAnimation() {
+// var $elem = $('.bar .level');
   
-  if($elem.hasClass('start')) return;
+//   if($elem.hasClass('start')) return;
   
-  if(isElementInViewport($elem)) {
-  $elem.addClass('start');
-  }
-}
+//   if(isElementInViewport($elem)) {
+//   $elem.addClass('start');
+//   }
+// }
 
-$(window).scroll(function(){
-checkAnimation();
-});
-
-
-// IntersectionObserverAPI
+// $(window).scroll(function(){
+// checkAnimation();
+// });
