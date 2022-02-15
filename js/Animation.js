9f2e9d423914ceb122a5f3ea85ac716c638a3351
CatherineBaptist <92572194+CatherineBaptist@users.noreply.github.com>
2022-02-15 11:57:55 +0000

    Update Animation.js

--- js/Animation.js
+++ js/Animation.js
@@ -1,5 +1,26 @@
-on window scroll {
-if (scroll pos > x) {
-elements.addClass("TeamAnimation");
+function isElementInViewport(elem) {
+var $elem = $(elem);
+
+  var scrollElem = ((navigation.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+  var viewportTop = $(scrollElem).scrollTop();
+  var elemBottom = viewportTop + $(window).height();
+  
+  var elemTop = Math.round($elem.offset().top);
+  var elemBottom = elemTop + $elem.height();
+  
+  return ((elemTop < viewportBottom) && (elemBottom > viewTop));
 }
+
+function checkAnimation() {
+var $elem = $('.bar .level');
+  
+  if($elem.hasClass('start')) return;
+  
+  if(isElementInViewport($elem)) {
+  $elem.addClass('start');
+  }
 }
+
+$(window).scroll(function(){
+checkAnimation();
+});
