ba1e4c8a4d2ab18e12d406eb415fe8f88f8c28cf
CatherineBaptist <92572194+CatherineBaptist@users.noreply.github.com>
2022-02-15 09:48:24 +0000

    Update Animation.js

--- js/Animation.js
+++ js/Animation.js
@@ -1,22 +1,38 @@
-var sections = document.querySelectorAll('section');
-var options = {
-rootMargin: '0px',
-threshold: 0.25
-}
-var callback = (entries) => {
-  entries.forEach((entry) => {
-    var target = entry.target;
-    if (entry.intersectionRatio >= 0.25) {
-      target.classList.add("is-inview");
-    } else {
-      target.classList.remove("is-inview");
-    }
-  })
-}
-var observer = new intersectionObserver(callback, options)
-sections.forEach((section, index) => {
-  observer.observe(section)
-})
+const observer = new IntersectionObserver(entries => {
+entries.forEach(entry => {
+const section = entry.target.querySelector('section');
+  
+  if(entry.isIntersecting) {
+  section.classList.add('TeamAnimation');
+    return;
+  }
+  
+  section.classList.remove('TeamAnimation');
+});
+});
+
+observer.observe(document.querySelector((.Animation_container));
+
+
+// var sections = document.querySelectorAll('section');
+// var options = {
+// rootMargin: '0px',
+// threshold: 0.25
+// }
+// var callback = (entries) => {
+//   entries.forEach((entry) => {
+//     var target = entry.target;
+//     if (entry.intersectionRatio >= 0.25) {
+//       target.classList.add("is-inview");
+//     } else {
+//       target.classList.remove("is-inview");
+//     }
+//   })
+// }
+// var observer = new intersectionObserver(callback, options)
+// sections.forEach((section, index) => {
+//   observer.observe(section)
+// })
 
 // var container = document.querySelector('.Animation_container');
 // container.addEventListener('scroll', function () {
